cefd8a07809584b2908d55650f07dce1be13e412
Akashjindall <Akash.15132@stu.upes.ac.in>
2026-01-30 15:50:27 +0530

    Add files via upload

---
+++ Static3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+class static3 {
+// static int n;
+
+int n;
+public static void main(String [] args){
+Scanner s = new Scanner (System.in);
+static3 m = new static3();
+// int n;
+m.n = s.nextInt();
+// n = s.nextInt();
+// System.out.print(n);
+
+System.out.print(m.n);
+}
+}
