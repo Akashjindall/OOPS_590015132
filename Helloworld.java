3a7ecfcb772d55ffff761de88504ea1197f5702c
Akashjindall <Akash.15132@stu.upes.ac.in>
2026-02-01 11:43:59 +0530

    Refactor Helloworld class for simplified output
    Updated class name and simplified output to 'Hello World'. Removed unnecessary code.

--- Helloworld.java
+++ Helloworld.java
@@ -1,24 +1,17 @@
-// #include <stdio.h>
-
-// void main() {
-//     printf("Hello, World!");
-// }
-
-
-
-
-
-
-
-
-
-
-
-public class Helloworld {
+class HelloWorld {
     public static void main(String[] args) {
-        int num = 20;
-        for (int i = 1; i <= num; i++) {
-            System.out.println(i);
-        }
+        System.out.println("Hello World");
     }
 }
+
+
+
+// public class Helloworld {
+//     public static void main(String[] args) {
+//         int num = 20;
+//         for (int i = 1; i <= num; i++) {
+//             System.out.println(i);
+//         }
+//     }
+// }
+
