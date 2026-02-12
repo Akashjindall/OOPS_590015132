203d394f16557019795c774724e5fda88b1920d7
Akashjindall <Akash.15132@stu.upes.ac.in>
2026-02-13 00:04:30 +0530

    firstcode.java

--- firstcode.java
+++ firstcode.java
@@ -1,29 +1,3 @@
-// import java.util.scanner;
-
-// public class firstcode {
-//     public static void main (String[] args) {
-
-//         Scanner sc = new Scanner(System.in);
-
-//         int n = 5;
-//         String [] str = new String[n];
-
-//         for (int i = 0; i < n; i++) {
-//             str[i] = sc.nextLine();
-        
-//             for (string s : str) {
-//                 System.out.println(s);
-//             }
-
-//             Sc.close();
-//         }
-//     }
-// }
-
-
-
-
-
 import java.util.Scanner;
 
 public class firstcode {
@@ -48,6 +22,7 @@ public class firstcode {
     }
     
 }
+
 
 
 
