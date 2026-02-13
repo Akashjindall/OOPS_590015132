e7f1cef5796d0e65c784cb57aa1f64fd2af21c9e
Akashjindall <Akash.15132@stu.upes.ac.in>
2026-02-13 22:20:04 +0530

    add comments

--- Static3.java
+++ Static3.java
@@ -1,18 +1,14 @@
 import java.util.Scanner;
-
-
 class static3 {
-// static int n;
-
-int n;
-public static void main(String [] args){
-Scanner s = new Scanner (System.in);
-static3 m = new static3();
-// int n;
-m.n = s.nextInt();
-// n = s.nextInt();
-// System.out.print(n);
-
-System.out.print(m.n);
-}
+    //  static int n;
+    // int n;
+     public static void main(String [] args){
+        Scanner s = new Scanner (System.in);
+        // static2 m = new static2();
+        int n;
+        // m.n = s.nextInt()
+        n = s.nextInt();
+        System.out.print(n);
+        // System.out.print(m.n);
+    }
 }
